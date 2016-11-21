f4884a13da89c445f6758ba1d7d330b7dd351545
David Caloguerea <dcalogue@gmail.com>
2016-11-21 09:41:16 -0300

    Update httpsclient-particle.h

--- firmware/httpsclient-particle.h
+++ firmware/httpsclient-particle.h
@@ -20,8 +20,10 @@ extern TCPClient client;
 extern bool g_https_complete;
 extern uint32 g_bytes_received;
 extern const bool g_https_trace;
+	
+void (*callback_func) (char*, int);
 
-int httpsclientSetup(const char * host, const char * path);
+int httpsclientSetup(const char * host, const char * path, function callback_func);
 int httpsClientConnection(unsigned char * requestContent, uint32 msg_len,
 			  const char * msg);
 void httpsclientCleanUp();
